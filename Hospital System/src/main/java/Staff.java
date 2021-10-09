029c2437f789a11b1d0d44ddd0ef0adf59222d7c
Fion Yung <fion.yung@mail.utoronto.ca>
2021-10-09 16:04:31 -0400

    Created Staff

---
+++ Hospital System/src/main/java/Staff.java
@@ -0,0 +1,16 @@
+/** This file contains the class Staff and is used to get the staff information
+ *
+ */
+
+import java.util.*;
+
+public class Staff {
+    private String name;
+    private String gender;
+    private String role;
+    private String ID;
+
+    public Staff() {
+
+    }
+}
