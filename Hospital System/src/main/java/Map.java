f2afe15b69724259457eee1a144dce570315f7ba
Fion Yung <fion.yung@mail.utoronto.ca>
2021-10-09 15:57:17 -0400

    Created Map

---
+++ Hospital System/src/main/java/Map.java
@@ -0,0 +1,11 @@
+/** This file contains the map of the hospital and is used to hold all the rooms in the building
+ *
+ */
+
+import java.util.*;
+
+abstract class Map {
+    private HashMap<Integer, Integer> PatientRooms;
+    private HashMap<Integer, String> Rooms;
+
+}
