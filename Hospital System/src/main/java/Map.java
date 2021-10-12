7346687cb030b5e8a31e61ae21ea35da9e5a5253
Fion Yung <fion.yung@mail.utoronto.ca>
2021-10-12 19:53:22 -0400

    edited map

--- Hospital System/src/main/java/Map.java
+++ Hospital System/src/main/java/Map.java
@@ -2,13 +2,11 @@
  *
  */
 
-//import java.io.FileReader;
-import java.io.BufferedReader;
+/**import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
+import java.io.IOException; **/
 import java.util.*;
-//import java.io.IOException;
-//import java.io.BufferedReader;
+
 
 
 public class Map {
