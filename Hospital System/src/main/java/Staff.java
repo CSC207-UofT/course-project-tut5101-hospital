15e031b05311b8bc11e85294564d4d511e27e4f9
Fion Yung <fion.yung@mail.utoronto.ca>
2021-10-12 21:24:38 -0400

    Made StaffData and removed StaffMap

--- Hospital System/src/main/java/Staff.java
+++ Hospital System/src/main/java/Staff.java
@@ -7,7 +7,6 @@ import java.util.*;
 public class Staff {
     private String name;
     private String gender;
-    private String role;
     private Integer ID;
     private ArrayList<String> StaffInfo;
 
@@ -15,11 +14,10 @@ public class Staff {
      * Construct a Staff, giving them a name, gender, role and ID.
      *
      */
-    public Staff(String name, String gender, String role) {
+    public Staff(String name, String gender) {
 
         this.name = name;
         this.gender = gender;
-        this.role = role;
         this.StaffInfo = new ArrayList<>();
         StaffData m = new StaffData();
         this.ID = m.getStaffDatasize();
@@ -28,13 +26,11 @@ public class Staff {
     public void addtoStaffInfo() {
         StaffInfo.add(this.name);
         StaffInfo.add(this.gender);
-        StaffInfo.add(this.role);
     }
 
     public String getStaffInfo() {
         return "Staff Name: " + this.name + "\n" +
                 "Gender: " + this.gender + "\n" +
-                "Staff role: " + this.role + "\n" +
                 "ID Number: " + this.ID.toString();
     }
 
