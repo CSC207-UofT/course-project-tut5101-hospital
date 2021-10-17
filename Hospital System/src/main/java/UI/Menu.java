3966bd420931735a73aa364810d62e51e7540fc8
Jiajin Wu <jiajin.wu@mail.utoronto.ca>
2021-10-17 00:18:21 -0400

    I did a unit test for Menu.java. It shows the case where someone signs up and views their appointment.

--- Hospital System/src/main/java/UI/Menu.java
+++ Hospital System/src/main/java/UI/Menu.java
@@ -13,14 +13,72 @@ public class Menu{
         
     }public void greeter(){
         System.out.println("Welcome to RealFakeHospital");
-    }public void loginSignup(){
-        
+    }
+//    public void loginSignup(){
+//
+//        int hcn =0;
+//        System.out.println("Sign up or login(1/2)");
+//
+//        int c = s.nextInt();
+//        s.nextLine();
+//        if(c==2){
+//            boolean success = false;
+//
+//            do{
+//                System.out.println("Input HealthCardNumber");
+//                hcn = s.nextInt();
+//                s.nextLine();
+//                System.out.println("Input password");
+//                String ipt_pwd = s.nextLine();
+//                success= ls.logIn(hcn, ipt_pwd);
+//                if(!success){
+//                    System.out.println("Login failed, enter 1 to switch to sign up instead");
+//                    int k=s.nextInt();
+//                    s.nextLine();
+//                    if(k==1){
+//                        c=1;
+//                        break;
+//                    }
+//                }
+//            }while(!success);
+//
+//        }if(c==1){
+//            System.out.println("Input name");
+//            String name = s.nextLine();
+//            System.out.println("Input gender");
+//            String gender = s.nextLine();
+//            System.out.println("Input contact number");
+//            int ctctNum = s.nextInt();
+//            s.nextLine();
+//            System.out.println("Input health card number");
+//            hcn = s.nextInt();
+//            s.nextLine();
+//            System.out.println("Input password");
+//            String pwd = s.nextLine();
+//            ls.signUp(name, gender, ctctNum, hcn, pwd);
+//            //System.out.println(ls.check_if_patient_exists(hcn));
+//        }
+//       this.hcn=hcn;
+//
+//    }public void activities(){
+//        //System.out.println(ls.check_if_patient_exists(hcn));
+//        int c=1;
+//        System.out.println("Make or view appointments(1/2)");
+//        c = s.nextInt();
+//        s.nextLine();
+//        if(c==1){
+//            makeAppointment();
+//        }if(c==2){
+//            viewAppointment();
+//        }
+//    }
+    public void loginSignup(){
+
         int hcn =0;
         System.out.println("Sign up or login(1/2)");
-        
-        int c = s.nextInt();
-        s.nextLine();
-        if(c==2){
+
+        String c = s.nextLine();
+        if(c.equals("2")){
             boolean success = false;
 
             do{
@@ -35,13 +93,13 @@ public class Menu{
                     int k=s.nextInt();
                     s.nextLine();
                     if(k==1){
-                        c=1;
+                        c="1";
                         break;
                     }
                 }
             }while(!success);
-            
-        }if(c==1){
+
+        }if(c.equals("1")){
             System.out.println("Input name");
             String name = s.nextLine();
             System.out.println("Input gender");
@@ -57,20 +115,19 @@ public class Menu{
             ls.signUp(name, gender, ctctNum, hcn, pwd);
             //System.out.println(ls.check_if_patient_exists(hcn));
         }
-       this.hcn=hcn;
+        this.hcn=hcn;
 
     }public void activities(){
-        //System.out.println(ls.check_if_patient_exists(hcn));
-        int c=1;
-        System.out.println("Make or view appointments(1/2)");
-        c = s.nextInt();
-        s.nextLine();
-        if(c==1){
-            makeAppointment();
-        }if(c==2){
-            viewAppointment();
+            //System.out.println(ls.check_if_patient_exists(hcn));
+            String c="1";
+            System.out.println("Make or view appointments(1/2)");
+            c = s.nextLine();
+            if(c.equals("1")){
+                makeAppointment();
+            }if(c.equals("2")){
+                viewAppointment();
+            }
         }
-    }
     private void makeAppointment() {
         System.out.println(ls.check_if_patient_exists(hcn));
         ScheduleManager sm = new ScheduleManager(ls.initPatient(hcn));
