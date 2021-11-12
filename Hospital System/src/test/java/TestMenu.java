733f59b377d6b920652e80e89a24ad6c76b77c1d
Fion Yung <fion.yung@mail.utoronto.ca>
2021-11-11 21:38:01 -0500

    Added new tests

--- Hospital System/src/test/java/TestMenu.java
+++ Hospital System/src/test/java/TestMenu.java
@@ -1,3 +1,5 @@
+import Patients.PatientData;
+import Patients.Patient;
 import Schedule.ScheduleManager;
 import UI.Menu;
 import UseCases.LoginSignup;
@@ -8,16 +10,31 @@ import org.junit.Test;
 public class TestMenu {
 
     private LoginSignup loginSignup;
+    private PatientData patientData;
+    private Patient patient;
+    private ScheduleManager scheduleManager;
+
 
     @Before
     public void setUp() throws Exception {
         loginSignup = new LoginSignup();
         loginSignup.signUp("Harry", "Male", 1, 2, "3");
+        patientData = new PatientData();
+        patient = new Patient("Harry", "Male", 1, 2, "3");
+        scheduleManager = new ScheduleManager(patient);
+
     }
 
     @Test
     public void TestLoginSignUpHCNexist() {
         Assert.assertTrue("Health card should be added into the system as part of the signup", loginSignup.check_if_patient_exists(2));
+    }
+
+    @Test
+    public void TestPatientDatacontainsPatient() {
+        Assert.assertEquals("Patient information should be stored in Patient Data",
+                "Patient Name: Harry" + "\n" + "Gender: Male" + "\n" + "Contact Number: 1" + "\n" + "Health Card Number: 2",
+                patientData.viewPatient(patient));
     }
 
 
