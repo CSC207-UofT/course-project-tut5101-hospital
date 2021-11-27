8fc71456e8686648164ab6ce93ec88d1c3069d83
guyuli2002 <tom.gu@mail.utoronto.ca>
2021-11-26 22:26:59 -0500

    Add javadoc

--- Hospital System/src/main/java/Controllers/LoginSignUp/CheckIfExists.java
+++ Hospital System/src/main/java/Controllers/LoginSignUp/CheckIfExists.java
@@ -4,13 +4,25 @@ import UseCases.Patient.PatientManager;
 import UseCases.Patient.PatientManaging;
 import UseCases.Staff.StaffManager;
 
-//since this will not be used in upper level classes, we do not have to implement Finish. This class acts like a library of methods for SignUp classes to use.
 public class CheckIfExists {
+    /**
+     * Constructor of check if exist controller,since this will not be used in upper level classes,
+     * we do not have to implement Finish. This class acts like a library of methods for SignUp classes to use.
+     *
+     * @param hcn
+     * @return
+     */
     public boolean checkPatExists(int hcn) {
         PatientManaging patientManaging = new PatientManager();
         return patientManaging.checkIfPateintExist(hcn);
     }
 
+    /**
+     * Check if a staff exist giving an id of the staff.
+     *
+     * @param id
+     * @return
+     */
     public boolean checkStaffExists(int id) {
         return new StaffManager().checkIfStaffExist(id);
     }
