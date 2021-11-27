70670dbaae181d262d38a48f60c12e6172161571
guyuli2002 <tom.gu@mail.utoronto.ca>
2021-11-27 02:00:00 -0500

    error

--- Hospital System/src/main/java/Exceptions/InvalidInputException.java
+++ Hospital System/src/main/java/Exceptions/InvalidInputException.java
@@ -1,8 +1,12 @@
 package Exceptions;
 
 
-public class InvalidInputException extends Exception{
-    public InvalidInputException(String e){
+public class InvalidInputException extends Exception {
+    /**
+     * Exceptions
+     * @param e
+     */
+    public InvalidInputException(String e) {
         super(e);
         System.out.println("check your input it is invalid.");
     }
