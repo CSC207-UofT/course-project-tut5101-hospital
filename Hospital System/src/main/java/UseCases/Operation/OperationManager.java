c58e6be296d0cc6d8e60b4f118728eb0b1cd9eb8
guyuli2002 <tom.gu@mail.utoronto.ca>
2021-11-28 19:58:25 -0500

    No change

--- Hospital System/src/main/java/UseCases/Operation/OperationManager.java
+++ Hospital System/src/main/java/UseCases/Operation/OperationManager.java
@@ -18,7 +18,7 @@ public class OperationManager {
 
     /**
      * Constructor for operation Manager, reads in a patient or a doctor and manage scheduled and finished operations
-     * @param p 
+     * @param p
      */
     public OperationManager(Patient p) {
         this.scdlOps = p.getScdldOps();
