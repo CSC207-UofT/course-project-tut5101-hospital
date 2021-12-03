c09a641b3de1e2b14b5624504dc6bfa6cf3d776b
Jiajin Wu <jiajin.wu@mail.utoronto.ca>
2021-12-03 17:54:00 -0500

    main

--- Hospital System/src/main/java/Main.java
+++ Hospital System/src/main/java/Main.java
@@ -3,7 +3,7 @@ import UI.MenuChooseStaffOrPatient;
 import UI.MenuForPatient;
 
 public class Main {
-    public static void main(String[] args) throws InvalidInputException {
+    public static void main(String[] args){
         MenuChooseStaffOrPatient menuChooseStaffOrPatient = new MenuChooseStaffOrPatient();
         menuChooseStaffOrPatient.choosePatientOrStaff();
     }
