1d77f135f5c0e905f4efe024ed2d20d0298eae24
Fion Yung <fion.yung@mail.utoronto.ca>
2021-12-04 21:49:04 -0500

    Added comments and created tests

--- Hospital System/src/test/java/UseCases/TestOperationManager.java
+++ Hospital System/src/test/java/UseCases/TestOperationManager.java
@@ -3,29 +3,47 @@ package UseCases;
 import Entity.Operations.Operation;
 import Entity.Patients.Patient;
 import Entity.Staff.Doctor;
+import UseCases.Operation.OperationFacade.OperationBonusCalculator;
+import UseCases.Operation.OperationFacade.OperationFeeCalculator;
 import UseCases.Operation.OperationManager;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.time.format.DateTimeFormatter;
+
 public class TestOperationManager {
-    private Operation operation;
-    private Doctor doctor;
-    private Patient patient;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     @Before
     public void setUp() throws Exception {
+        List<LocalDateTime> optTime = new ArrayList<>();
+        LocalDateTime start = LocalDateTime.parse("2021-10-01 10:00", formatter);
+        optTime.add(start);
+        LocalDateTime end = LocalDateTime.parse("2021-10-01 12:00", formatter);
+        optTime.add(end);
+        Doctor doctor = new Doctor();
+        Patient patient = new Patient();
+        Operation operation = new Operation(100, "Appendicitis", "Appendix removal", optTime);
         OperationManager opPatient = new OperationManager(patient);
         OperationManager opDoctor = new OperationManager(doctor);
+        OperationBonusCalculator opBonus = new OperationBonusCalculator(doctor, operation);
+        OperationFeeCalculator opFee = new OperationFeeCalculator(patient, operation);
+
     }
 
     @Test
     public void TestOperationFeeCalculator() {
-        Assert.assertEquals();
+        Assert.assertEquals(opFee.finish(), 100);
     }
 
     @Test
     public void TestOperationBonusCalculator() {
-        Assert.assertEquals();
+        Assert.assertEquals(opBonus.finish(), 20);
     }
+
+
 }
