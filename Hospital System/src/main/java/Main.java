57c6b9fe954784bfb956e6816b60fb6c4607d600
Jiajin Wu <jiajin.wu@mail.utoronto.ca>
2021-12-06 17:22:21 -0500

    doctor confirm appointment

--- Hospital System/src/main/java/Main.java
+++ Hospital System/src/main/java/Main.java
@@ -3,7 +3,7 @@ import Exceptions.StaffNotFoundException;
 import UI.MenuChooseStaffOrPatient;
 
 public class Main {
-    public static void main(String[] args) throws InvalidInputException, StaffNotFoundException {
+    public static void main(String[] args) throws InvalidInputException{
         MenuChooseStaffOrPatient menuChooseStaffOrPatient = new MenuChooseStaffOrPatient();
         menuChooseStaffOrPatient.choosePatientOrStaff();
     }
